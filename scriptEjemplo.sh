67506b4b4e8182b3eba0917eae6a951e79af89ca
Luis Romero <pes4mastermilan@gmail.com>
2022-09-30 00:15:20 -0300

    Add files via upload

---
+++ scriptEjemplo.sh
@@ -0,0 +1,51 @@
+FECHA_ACTUAL=$(date +"%d-%b-%y")
+echo $FECHA_ACTUAL
+# Fecha actual del Sistema
+
+FECHA_ACTUAL_EXT=$(date "+%d-%m-%y_%H-%M-%S")
+echo $FECHA_ACTUAL_EXT
+# Fecha actual extendida del Sistema
+
+HORA_ACTUAL=$(date +"%H-%M")
+echo $HORA_ACTUAL
+# Hora actual del Sistema
+
+FECHA_SISTEMA_AN=$(date +"%d-%b-%y")
+echo $FECHA_SISTEMA_AN
+# Almacenar la fecha  Alfanumérica actual del Equipo
+
+FECHA_SISTEMA_N=$(date +"%d-%m-%y")
+echo $FECHA_SISTEMA_N
+# Almacenar la fecha Numérica actual del Equipo
+
+DIA_ACTUAL=$(date +"%d")
+echo $DIA_ACTUAL
+# Almacenar el día actual del Equipo
+
+MES_ACTUAL_A=$(date +"%b")
+echo $MES_ACTUAL_A
+# Almacenar el mes alfabético actual del Equipo
+
+MES_ACTUAL_N=$(date +"%m")
+# echo $MES_ACTUAL_N
+# Almacenar el mes numérico actual del Equipo
+
+ANO_ACTUAL_2C=$(date +"%y")
+# echo $ANO_ACTUAL_2C
+# Almacenar el año (con 2 cifras) actual del Equipo
+
+ANO_ACTUAL_4C=$(date +"%Y")
+# echo $ANO_ACTUAL_4C
+# Almacenar el año (con 4 cifras) actual del Equipo
+
+HORA_SISTEMA=$(date +"%H-%M")
+# echo $HORA_SISTEMA
+# Almacenar la Hora actual del Equipo
+
+HORA_ACTUAL=$(date +"%H")
+# echo $HORA_ACTUAL
+# Almacenar la Hora actual del Equipo
+
+MINUTOS_ACTUAL=$(date +"%M")
+# echo $MINUTOS_ACTUAL
+# Almacenar la Hora actual del Equipo
